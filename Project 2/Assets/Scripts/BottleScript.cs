2467daa063c65830b4355b72e5b3787d057c2525
Gkwako <gloria.kwako@hva.nl>
2023-06-12 16:30:18 +0200

    12juni

---
+++ Project 2/Assets/Scripts/BottleScript.cs
@@ -0,0 +1,32 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using TMPro;
+
+public class BottleScript : MonoBehaviour
+{
+    private GameObject selectedObject; // Reference to the selected object
+    public float speed = 5f; // Speed of the bottle
+    public GameObject dialogueBox;
+
+    void Update()
+    {
+        // Move the bottle to the right
+        transform.Translate(Vector3.left * Time.deltaTime);
+    }
+
+    public void OnMouseButtonDown()  
+    {  
+         if (Input.GetMouseButtonUp(0))
+        {
+            dialogueBox.gameObject.SetActive(true);
+            // isDragging = false;
+            selectedObject = null; // Reset the selected object when the mouse button is released
+        }
+
+        // DialogueManager.Instance.ActivateDialogueBox();  
+    }
+    
+    
+}
--- Project 2/Assets/Scripts/BottleTrigger.cs
+++ Project 2/Assets/Scripts/BottleTrigger.cs
@@ -4,28 +4,33 @@ using UnityEngine;
 using UnityEngine.UI;
 using TMPro;
 
-public class IntroDialogue : MonoBehaviour
+public class BottleTrigger : MonoBehaviour
 {
     public TextMeshProUGUI textComponent;
     public GameObject dialogueBox;
     public string[] lines;
     public float textSpeed;
-    public float invokeTime;
+    // public float invokeTime;
     
     private int index;
     float nextTypetime;
     float delay = 10f;
 
+    private GameObject selectedObject; // Reference to the selected object
+    public float speed = 5f; // Speed of the bottle
+
     void Start()
     {
         dialogueBox.gameObject.SetActive(false);
         textComponent.text = string.Empty;
-        Invoke("StartDialogue",invokeTime);
+        Invoke("StartDialogue");
     }
 
     // Update is called once per frame
     void Update()
     {
+        transform.Translate(Vector3.left * Time.deltaTime);
+
         if (Time.time > nextTypetime)
         {
             if(textComponent.text == lines[index])
@@ -44,12 +49,20 @@ public class IntroDialogue : MonoBehaviour
 
     void StartDialogue()
     {
+        if(Input.GetMouseButtonUp(0)){
+            dialogueBox.gameObject.SetActive(true);
+        }
         if(dialogueBox.gameObject.activeInHierarchy == false)
         {
             dialogueBox.gameObject.SetActive(true);
             index = 0;
             StartCoroutine(TypeLine());
         }
+    }
+
+    void OnMouseDown()
+    {
+        StartDialogue();
     }
 
     IEnumerator TypeLine()
