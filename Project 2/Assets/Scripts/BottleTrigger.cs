b555a6cc093d6655c69a689b9e9ce1f84020a5e8
Gkwako <gloria.kwako@hva.nl>
2023-06-13 10:41:38 +0200

    ...

--- Project 2/Assets/Scripts/BottleTrigger.cs
+++ Project 2/Assets/Scripts/BottleTrigger.cs
@@ -1,90 +1,90 @@
-using System.Collections;
-using System.Collections.Generic;
-using UnityEngine;
-using UnityEngine.UI;
-using TMPro;
+// using System.Collections;
+// using System.Collections.Generic;
+// using UnityEngine;
+// using UnityEngine.UI;
+// using TMPro;
 
-public class BottleTrigger : MonoBehaviour
-{
-    public TextMeshProUGUI textComponent;
-    public GameObject dialogueBox;
-    public string[] lines;
-    public float textSpeed;
-    // public float invokeTime;
+// public class BottleTrigger : MonoBehaviour
+// {
+//     public TextMeshProUGUI textComponent;
+//     public GameObject dialogueBox;
+//     public string[] lines;
+//     public float textSpeed;
+//     // public float invokeTime;
     
-    private int index;
-    float nextTypetime;
-    float delay = 10f;
+//     private int index;
+//     float nextTypetime;
+//     float delay = 10f;
 
-    private GameObject selectedObject; // Reference to the selected object
-    public float speed = 5f; // Speed of the bottle
+//     private GameObject selectedObject; // Reference to the selected object
+//     public float speed = 5f; // Speed of the bottle
 
-    void Start()
-    {
-        dialogueBox.gameObject.SetActive(false);
-        textComponent.text = string.Empty;
-        Invoke("StartDialogue");
-    }
+//     void Start()
+//     {
+//         dialogueBox.gameObject.SetActive(false);
+//         textComponent.text = string.Empty;
+//         Invoke("StartDialogue");
+//     }
 
-    // Update is called once per frame
-    void Update()
-    {
-        transform.Translate(Vector3.left * Time.deltaTime);
+//     // Update is called once per frame
+//     void Update()
+//     {
+//         transform.Translate(Vector3.left * Time.deltaTime);
 
-        if (Time.time > nextTypetime)
-        {
-            if(textComponent.text == lines[index])
-            {
-                NextLine();
-                nextTypetime = Time.time + delay;
-            }
-            else
-            {
-                StopAllCoroutines();
-                textComponent.text = lines[index];
-                // nextTypetime = Time.time + delay;
-            }
-        } 
-    }
+//         if (Time.time > nextTypetime)
+//         {
+//             if(textComponent.text == lines[index])
+//             {
+//                 NextLine();
+//                 nextTypetime = Time.time + delay;
+//             }
+//             else
+//             {
+//                 StopAllCoroutines();
+//                 textComponent.text = lines[index];
+//                 // nextTypetime = Time.time + delay;
+//             }
+//         } 
+//     }
 
-    void StartDialogue()
-    {
-        if(Input.GetMouseButtonUp(0)){
-            dialogueBox.gameObject.SetActive(true);
-        }
-        if(dialogueBox.gameObject.activeInHierarchy == false)
-        {
-            dialogueBox.gameObject.SetActive(true);
-            index = 0;
-            StartCoroutine(TypeLine());
-        }
-    }
+//     void StartDialogue()
+//     {
+//         if(Input.GetMouseButtonUp(0)){
+//             dialogueBox.gameObject.SetActive(true);
+//         }
+//         if(dialogueBox.gameObject.activeInHierarchy == false)
+//         {
+//             dialogueBox.gameObject.SetActive(true);
+//             index = 0;
+//             StartCoroutine(TypeLine());
+//         }
+//     }
 
-    void OnMouseDown()
-    {
-        StartDialogue();
-    }
+//     void OnMouseDown()
+//     {
+//         StartDialogue();
+//     }
 
-    IEnumerator TypeLine()
-    {
-        foreach (char c in lines[index].ToCharArray())
-        {
-            textComponent.text += c;
-            yield return new WaitForSeconds(textSpeed);
-        }
-    }
+//     IEnumerator TypeLine()
+//     {
+//         foreach (char c in lines[index].ToCharArray())
+//         {
+//             textComponent.text += c;
+//             yield return new WaitForSeconds(textSpeed);
+//         }
+//     }
     
-    void NextLine()
-    {
-        if(index < lines.Length - 1)
-        {
-            index++;
-            textComponent.text = string.Empty;
-            StartCoroutine(TypeLine());
-        } 
-        else
-        {
-            dialogueBox.gameObject.SetActive(false);
-        } 
-    }
-}
+//     void NextLine()
+//     {
+//         if(index < lines.Length - 1)
+//         {
+//             index++;
+//             textComponent.text = string.Empty;
+//             StartCoroutine(TypeLine());
+//         } 
+//         else
+//         {
+//             dialogueBox.gameObject.SetActive(false);
+//         } 
+//     }
+// }
